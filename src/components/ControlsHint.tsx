d2be4968b0c2fb37a2928bd9400719483de94ffc
MoeD <ulyssemdbh@gmail.com>
2026-04-06 21:31:57 +0200

    Basic ControlsHint component implementation.

---
+++ src/components/ControlsHint.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ControlsHint = () => {
+    return (
+        <div className="controls-hint">
+            {/* Controls instructions for the player will be displayed here */}
+        </div>
+    );
+};
+
+export default ControlsHint;
