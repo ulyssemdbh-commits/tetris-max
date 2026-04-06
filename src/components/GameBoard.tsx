5cafc6b749f81e5e7c943a22eb1545de59b08096
MoeD <ulyssemdbh@gmail.com>
2026-04-06 21:31:55 +0200

    Basic GameBoard component implementation.

---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GameBoard = () => {
+    return (
+        <div className="game-board">
+            {/* Game grid of 10x20 will be rendered here */}
+        </div>
+    );
+};
+
+export default GameBoard;
