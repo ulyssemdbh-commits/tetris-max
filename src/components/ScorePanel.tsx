0fdca765141321fb434e9b424d5cb5529b237621
MoeD <ulyssemdbh@gmail.com>
2026-04-06 21:31:56 +0200

    Basic ScorePanel component implementation.

---
+++ src/components/ScorePanel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ScorePanel = () => {
+    return (
+        <div className="score-panel">
+            {/* Score, level, and lines will be displayed here */}
+        </div>
+    );
+};
+
+export default ScorePanel;
