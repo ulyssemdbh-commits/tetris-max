0a704343a682f2d3e799c06c2dae35608d494099
MoeD <ulyssemdbh@gmail.com>
2026-04-06 21:32:00 +0200

    Defined constants for the game.

---
+++ src/game/constants.ts
@@ -0,0 +1,7 @@
+export const BOARD_WIDTH = 10;
+export const BOARD_HEIGHT = 20;
+export const TETROMINOS = [
+    { shape: [['X', 'X', 'X', 'X']], color: 'cyan' }, // I
+    { shape: [['X', 'X'], ['X', 'X']], color: 'yellow' }, // O
+    // 5 more tetrominos...
+];
